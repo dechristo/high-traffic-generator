556e6c0c3dd429f726717a9f9557fbb6ec19250a
Luiz Eduardo de Christo <ChristoL@ITs-MacBook-Pro.local>
2019-09-25 09:42:28 +0200

    Add threads and queues

--- simulate.py
+++ simulate.py
@@ -1,14 +1,30 @@
-import multiprocessing as mp
+from threading import Thread
+import queue
 import requests
 
-#pool = mp.Pool(mp.cpu_count())
+concurrent_requests = 200
+num_threads = 4
 
-# result = [pool.apply(requests
-#            .post('http://localhost:3000/'),
-#            data={"query": "{getCitiesByPostCode(postCode: \"12107\"){cityName}}"}) for i in range(2)]
 
-#pool.close()
+def make_request(q):
+    while not q.empty():
+        current = q.get()
+        print(f'request {current} sent.')
+        requests.post('http://localhost:3000/applications/telco/graphql',
+            json={'query': '{getCitiesByPostCode(postCode: "12107"){cityName}}'})
+        q.task_done()
 
-result = requests.post('http://localhost:3000/', json={'query': '{getCitiesByPostCode(postCode: "12107"){cityName}}'})
 
-print(result)
+q = queue.Queue(concurrent_requests * num_threads)
+
+for index in range(concurrent_requests):
+    q.put(index)
+
+for threadIndex in range(num_threads):
+    t = Thread(target=make_request, args=(q,))
+    t.daemon = True
+    t.start()
+
+q.join()
+
+
